abcc8273f1c0676a7dec0623b24ca7446753d3c9
Nghia Nguyen <quangnghiadev@gmail.com>
2018-04-13 00:36:59 +0700

    make it better

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://qnghiadev:admin123@ds117956.mlab.com:17956/teamchat-db",
   "bodyLimit": "100kb"
 }
