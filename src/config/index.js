028b419e3c406c9b83c1bcdca2d2e41cc4e89c7d
Nghia Nguyen <quangnghiadev@gmail.com>
2018-04-13 20:29:47 +0700

    change mongodb url

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": "mongodb://admin:admin123@ds117956.mlab.com:17956/teamchat-db",
+  "mongoUrl": "mongodb://admindb:123456@ds117956.mlab.com:17956/teamchat-db",
   "bodyLimit": "100kb"
 }
